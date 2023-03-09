57b3793da5efe0d313f18411fd15cf46c06f8c39
Muzammil <muzammilvptnr@gmail.com>
2023-03-09 15:23:48 +0530

    module created

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Stock Warning Email',
+    'version': '16.0.1.0.0',
+    'sequence': '-11',
+    'category': 'inventory',
+    'summary': 'Sent Stock Warning Email',
+    'description': 'Sent Stock Warning Email',
+
+    'installation': True,
+    'application': True,
+
+    'depends': ['base'],
+    'data': [
+    ],
+    'assets': {
+       'point_of_sale.assets': [
+       ],
+}
+}
